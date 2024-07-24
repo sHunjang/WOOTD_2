92645dc13fa11966a83cb083d7e69c0840cb45aa
SH <seunghunjang@SH.local>
2024-07-24 11:11:06 +0900

    feat : code fix

--- Wannabe_Style_Similarity.py
+++ Wannabe_Style_Similarity.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 
 # 데이터
-data = [69, 21, 26, 35]
+data = [82, 15, 20, 33]
 
 # 그래프 그리기
 plt.figure(figsize=(8, 6))
-plt.bar(range(len(data)), data, color='green', edgecolor='none')
+plt.bar(range(len(data)), data, color='#206aab', edgecolor='none')
 plt.title('Epoch 40 Model')
 plt.xlabel('Combination')
 plt.ylabel('Similairty')
 plt.legend()
 plt.grid(False)
 plt.xticks(range(len(data)))
+plt.ylim(0, 100)
 plt.show()
